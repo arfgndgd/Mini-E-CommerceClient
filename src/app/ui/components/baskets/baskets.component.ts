b1c543cd79a53c5cead221bdb9f70544b1401d16
arfgndgd <gundogduarif4@gmail.com>
2025-03-08 21:50:39 +0300

    component bases designed

---
+++ src/app/ui/components/baskets/baskets.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-baskets',
+  standalone: false,
+  templateUrl: './baskets.component.html',
+  styleUrl: './baskets.component.css'
+})
+export class BasketsComponent {
+
+}
