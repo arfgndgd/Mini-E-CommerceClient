3353c2cbc40a83b9b6116bc0478a96a88e2379af
arfgndgd <gundogduarif4@gmail.com>
2025-03-19 23:40:41 +0300

    spinner add file upload

--- src/app/services/common/file-upload/file-upload.component.ts
+++ src/app/services/common/file-upload/file-upload.component.ts
@@ -7,6 +7,8 @@ import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../../ui
 import { MatDialog } from '@angular/material/dialog';
 import { FileUploadDialogComponent, FileUploadDialogState } from '../../../dialogs/file-upload-dialog/file-upload-dialog.component';
 import { DialogService } from '../dialog.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { SpinnerType } from '../../../base/base.component';
 
 @Component({
   selector: 'app-file-upload',
@@ -20,7 +22,8 @@ export class FileUploadComponent {
     private alertifyService: AlertifyService,
     private customToastrService: CustomToastrService, 
     private dialog: MatDialog, 
-    private dialogService: DialogService
+    private dialogService: DialogService,
+    private spinner: NgxSpinnerService
   ){}
 
   public files: NgxFileDropEntry[];
@@ -39,6 +42,7 @@ export class FileUploadComponent {
       componentType: FileUploadDialogComponent,
       data: FileUploadDialogState.Yes,
       afterClosed: () => {
+        this.spinner.show(SpinnerType.BallAtom)
         this.httpClientService.post({
           controller: this.options.controller,
           action: this.options.action,
@@ -47,6 +51,8 @@ export class FileUploadComponent {
         }, fileData).subscribe(data => {
           
           const message: string = "Dosyalar başarıyla yüklenmiştir."
+          
+          this.spinner.hide(SpinnerType.BallAtom);
           if (this.options.isAdminPage) {
             this.alertifyService.message(message, {
               dissmissOthers: true,
@@ -59,8 +65,11 @@ export class FileUploadComponent {
               position: ToastrPosition.TopRight
             })
           }
+
         },(errorResponse: HttpErrorResponse) => {
           const message: string = "Dosyalar yüklenirken beklenmedik bir hata ile karşılaşılmıştır."
+         
+          this.spinner.hide(SpinnerType.BallAtom);
           if (this.options.isAdminPage) {
             this.alertifyService.message(message, {
               dissmissOthers: true,
